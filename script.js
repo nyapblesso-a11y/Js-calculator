e07d7483245d4215d261996ed32258ab19f719d0
nyapblesso-a11y <nyapblesso@gmail.com>
2025-09-28 15:03:19 +0100

    final commit of my css, html, and script

---
+++ script.js
@@ -0,0 +1,20 @@
+const display = document.getElementById("display");
+let operator = document.getElementsByClassName("button");
+function clearDisplay() {
+  display.value = "";
+}
+
+function appendToDisplay(elem) {
+  display.value += elem;
+}
+
+function Calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error!";
+  }
+}
+function deleteInput() {
+  display.value =display.value.toString().slice(0, -1);
+}
